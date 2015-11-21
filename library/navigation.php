e5aa3d2f6ad2a23b7f81e32a95405a2936ba08b7
Nude Mutant <nudemutant@gmail.com>
2015-11-21 14:32:46 -0600

    Menu Walker to render MLM html

--- library/navigation.php
+++ library/navigation.php
@@ -92,13 +92,14 @@ if ( ! function_exists( 'multi_level_menu' ) ) {
             'container' => false,                           // Remove nav container
             'container_class' => '',                        // Class of container
             'menu' => '',                                   // Menu name
-            'menu_class' => '',           // Adding custom nav class
-            'theme_location' => '',                // Where it's located in the theme
+            'menu_class' => 'menu__wrap',                   // Adding custom nav class
+            'theme_location' => '',                         // Where it's located in the theme
             'before' => '',                                 // Before each link <a>
             'after' => '',                                  // After each link </a>
             'link_before' => '',                            // Before each link text
             'link_after' => '',                             // After each link text
-            'depth' => 5,                                   // Limit the depth of the nav
+            'items_wrap' => '<div id="%1$s" class="%2$s">%3$s</div>',// Replacing the ul with div
+            'depth' => 0,                                   // Limit the depth of the nav
             'fallback_cb' => false,                         // Fallback function (see below)
             'walker' => new Mutantpress_Multi_Level_Menu_Walker(),
         ));
